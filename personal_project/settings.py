08ab95be6a45e7808b048bfb7d201eed823e40be
ash_walunj <ashwinwalunj@gmail.com>
2021-07-06 12:21:18 +0530

    done

--- personal_project/settings.py
+++ personal_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
 
 django_heroku.settings(locals())
 # Default primary key field type
